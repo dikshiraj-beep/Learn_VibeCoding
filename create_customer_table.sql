9dc704fb16101168d50aab1e2ba22c16a3e1fdc8
Dheeba <dikshiraj@gmail.com>
2026-07-18 17:40:31 +0800

    Create SQL table

---
+++ create_customer_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customer (
+    Customer_ID INT PRIMARY KEY,
+    First_Name VARCHAR(100),
+    Last_Name VARCHAR(100),
+    MemberID VARCHAR(50),
+    Expiration DATE,
+    Books_CheckedOut INT,
+    Member_Since DATE
+);
